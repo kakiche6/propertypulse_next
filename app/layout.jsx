95d4bc43de348214f309f25f361092f032534a98
kakiche6 <arezki.mezrag@gmail.com>
2024-03-20 23:23:38 -0400

    session provider wrapper

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,25 @@
 import "@/assets/styles/globals.css";
+import AuthProvider from "@/components/AuthProvider";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "PropertyPulse | Find the perfect rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
 };
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
